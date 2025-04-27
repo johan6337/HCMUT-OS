1952c255209ab0f8878bda046d8d0eda3c1a0029
johan6337 <139458607+johan6337@users.noreply.github.com>
2025-04-27 03:18:27 +0000

    uncomment the debug marco for mem

--- include/os-cfg.h
+++ include/os-cfg.h
@@ -6,9 +6,9 @@
 
 #define MM_PAGING
 // #define MM_FIXED_MEMSZ
-// #define VMDBG 1
+#define VMDBG 1
 #define MMDBG 1
-// #define IODUMP 1
-// #define PAGETBL_DUMP 1
+#define IODUMP 1
+#define PAGETBL_DUMP 1
 
 #endif
